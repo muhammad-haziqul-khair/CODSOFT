23fa73b4396c41c664633c6192eaef952b551202
muhammad-haziqul-khair <mhaziqulkhair93@gmail.com>
2024-01-08 23:57:21 +0500

    configured user friendly environment

--- Task3(password_generator)/password_generator_cmd.py
+++ Task3(password_generator)/password_generator_cmd.py
@@ -11,3 +11,57 @@ def generate_password(length):
     for password_character in range(length):
         password += random.choice(char_lst)
     print(password)
+
+def get_length_of_pswd():
+    global length
+    while True:
+        try:
+            length = int(input("Enter the length of Password: "))
+            if length > 0:
+                return length
+            else:
+                print("Length must be greater than 0.")
+            break
+        except ValueError:
+            print("Enter Valid Input!")
+
+def main():
+    length = get_length_of_pswd()
+    print()
+    print("",end="\t\t")
+    generate_password(length)
+    print()
+    print("",end="\t\t")
+
+    while True:
+        prompt = input("""Options:
+        1. Generate another password
+        2. Change the length of the password
+        0. Exit
+        Choose an option (0, 1, 2): """)
+        if prompt == "1":
+            print()
+            print("",end="\t\t")
+            generate_password(length)
+            print()
+            print("",end="\t\t")
+        elif prompt == "2":
+            length = get_length_of_pswd()
+            print()
+            print("",end="\t\t")
+            generate_password(length)
+            print()
+            print("",end="\t\t")
+        elif prompt == "0":
+            break
+        else:
+            print()
+            print("",end="\t\t")
+            print("Invalid Input!")
+            print()
+            print("",end="\t\t")
+
+    print("\nThank you!")
+
+if __name__ == "__main__":
+    main()
