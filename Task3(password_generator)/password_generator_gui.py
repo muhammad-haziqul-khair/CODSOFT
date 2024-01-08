edbb17db3b20f279eaa7163f8e2f0d90874c2fe2
muhammad-haziqul-khair <mhaziqulkhair93@gmail.com>
2024-01-09 01:39:09 +0500

    added button for pasword geneartion and password label

--- Task3(password_generator)/password_generator_gui.py
+++ Task3(password_generator)/password_generator_gui.py
@@ -1,6 +1,24 @@
 from tkinter import *
+import string
+import random
 
-def validate_input(num):
+#generates a random password
+def generate_password():
+    global length
+    length = length_var.get()
+    characters = str(string.ascii_letters+string.digits+string.punctuation)
+    char_lst = []
+    for char in characters:
+        char_lst.append(char)
+    random.shuffle(char_lst)
+    global password,pswd_label
+    password = ""
+    for password_character in range(length):
+        password += random.choice(char_lst)
+    pswd_label.config(text=password)
+
+#validates input to be integer only
+def validate_input(num):  
     if num.isdigit():
         return True
     elif num == "":
@@ -19,9 +37,15 @@ reg = root.register(validate_input)
 lb = Label(root,text = "Enter the length of Password: ",font=("Arial", 15, "bold"),bg ="#2B98DC",fg="white")
 lb.grid(row=2,column=1,padx=10, pady=10)
 
+# entry widget for getting password length
 length_var = IntVar()
 length_entry = Entry(root,width=10,font=("Arial", 20, "bold"),justify="center",bg ="white",textvariable=length_var,validate="key",validatecommand=(reg,"%P"))
 length_entry.grid(row=2,column=2,padx=10, pady=10)
 
+btn_pswd = Button(root,text="Generate Password",width = 20,font=7,command=generate_password)
+btn_pswd.grid(row=3,column=1,padx=10, pady=10)
+
+pswd_label = pswd_label = Label(root,text ="",font =("Arial",20,"bold"),width= 20,bg ="#2B98DC",fg="white")
+pswd_label.grid(row=4,column=1)
 
 root.mainloop()
